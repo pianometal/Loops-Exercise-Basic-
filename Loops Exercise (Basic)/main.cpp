bc506bfa94801e1537a3efcc9a91d63fd2763ce6
Kyle Lovely <kylesphere@gmail.com>
2020-11-28 13:23:36 -0500

    Update main.cpp

--- Loops Exercise (Basic)/main.cpp
+++ Loops Exercise (Basic)/main.cpp
@@ -1,14 +1,43 @@
-//
-//  main.cpp
-//  Loops Exercise (Basic)
-//
-//  Created by Kyle Lovely on 11/28/20.
-//
+/*
+
+ Another coding challenge from HackerRank
+ 
+ *Task
+ 
+ Given an integer, n, print its first 10 multiples (n * i). Each multiple (n * i) (where 1 <= i <= 10) should be printed on a new line in the form: (n * i) = result.
+ 
+ *Input Format
+ 
+ A single integer, n.
+ 
+ *Constraints
+ 
+ 2 <= n <= 20
+
+ *Output Format
+ 
+ Print 10 lines of output; each line i (where 1 <= i <= 10) contains the result of (n * i) in the form:
+ (n * i) = result.
+ 
+ ----------
+ 
+ A great little exercise to keep up those for loop chops
+
+*/
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    for (int j = 1; j <= 10; j++) {
+        cout << n << " x " << j << " = " << (n * j) << endl;
+    }
+
     return 0;
 }
